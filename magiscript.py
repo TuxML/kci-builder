46e6cb5790277b295ddd25371205b396d8ba7876
Khaled <16012048@istic.univ-rennes1.fr>
2021-02-17 14:50:06 +0100

    - magicscript now does more stuff

--- magiscript.py
+++ magiscript.py
@@ -1,22 +1,30 @@
 #!usr/bin/env python3
+import subprocess
+import sys
 import tempfile
+from time import sleep
 
 
-def main():
+def main(b_env):
     print("Starting...")
     # create a temporary directory
     with tempfile.TemporaryDirectory() as directory:
         print('The created temporary directory is %s' % directory)
-        create_dockerfile(directory)
+        path = directory + '\Dockerfile'
+        create_dockerfile(path, b_env)
+        while 1:
+            sleep(1)
+        subprocess.call(['docker', 'build', directory])
 
 
-def create_dockerfile(path, build_env):
-    with open(path+"\Dockerfile", mode="x") as df:
+def create_dockerfile(path, b_env):
+    with open(path, mode="x") as df:
         df.write("FROM kci_base\n")
-        df.write("RUN apt-get update && apt-get install --no-install-recommends -y gcc-%d gcc-%d-plugin-dev\n" % (build_env,build_env))
-        df.write("RUN update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-%d 500\n" % build_env)
+        df.write("RUN apt-get update && apt-get install --no-install-recommends -y %s %s-plugin-dev\n" % (b_env, b_env))
+        df.write("RUN update-alternatives --install /usr/bin/gcc gcc /usr/bin/%s 500\n" % b_env)
         df.write("RUN apt-get update && apt-get install --no-install-recommends -y libc6-dev libcap-dev libcap-ng-dev libelf-dev libpopt-dev\n")
 
 
 if __name__ == '__main__':
-    main()
+    build_env = sys.argv[1]
+    main(build_env)
